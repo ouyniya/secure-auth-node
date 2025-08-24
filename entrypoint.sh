1b7c2d95e0a8f031897ad939626e8f2afb8f8d1a
niya-s <niya.somkerd@gmail.com>
2025-08-24 21:23:56 +0700

    fix: waiting for MySQL code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -e
 
+# รอ MySQL พร้อม
+echo "Waiting for MySQL..."
+until nc -z -v -w30 mysql 3306
+do
+  echo "Waiting for database connection..."
+  sleep 2
+done
+
 echo "Running Prisma migrations..."
 npx prisma migrate deploy
 
